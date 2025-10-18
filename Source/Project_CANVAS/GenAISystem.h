cfecf511356e17ed0c79b38f1e600dc21bebb5fc
Kazamapilot <kazamapilot@gmail.com>
2025-10-19 02:00:54 +0530

    OK SO GENAI SYSTEM -> IT WOULD NEED A MENU TO SELCT FROM AND WOULD ALSO NEED A VALIDATER TO VALIDATE IF IT COULD HAPPEN

---
+++ Source/Project_CANVAS/GenAISystem.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ScenePlan.h" 
+#include "HttpModule.h"
+#include "GenAISystem.generated.h"
+
+/**
+ * 
+ */
+//  A EVENT DRIVEN -> SEND IT TO SCENE BUILDER
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnThemeDataReady, const FEnhancedScenePlan&, Plan);
+UCLASS()
+class PROJECT_CANVAS_API UGenAISystem : public UObject
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnThemeDataReady OnThemeDataReady;
+
+	UPROPERTY(BlueprintCallable)
+	void RequestSceneChange();
+	// --- Internal HTTP Callback ---
+	void OnOllamaResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+	// We need a reference to our parser
+	UPROPERTY()
+	class UJsonParser* Parser;
+
+	// Helper function to build the final prompt for the LLM
+	FString ConstructMasterPrompt(FString UserPrompt);
+};
